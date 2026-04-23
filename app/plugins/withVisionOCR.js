d7440433ba233570cc4625e43a570841211d7395
Jason <89934445+leejason2025@users.noreply.github.com>
2026-04-23 00:42:06 -0700

    Store VisionOCR source in modules/ and copy via plugin

--- app/plugins/withVisionOCR.js
+++ app/plugins/withVisionOCR.js
@@ -1,10 +1,24 @@
 const { withXcodeProject } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function withVisionOCR(config) {
   return withXcodeProject(config, (config) => {
     const xcodeProject = config.modResults;
-    const targetUuid = xcodeProject.getFirstTarget().uuid;
+    const projectRoot = config.modRequest.projectRoot;
+    const iosDir = path.join(projectRoot, 'ios', 'CodesTCG');
+    const srcDir = path.join(projectRoot, '..', 'modules', 'VisionOCR');
 
+    // Copy files into ios/ so Xcode can find them
+    for (const name of ['VisionOCR.swift', 'VisionOCR.m']) {
+      const src = path.join(srcDir, name);
+      const dest = path.join(iosDir, name);
+      if (fs.existsSync(src)) {
+        fs.copyFileSync(src, dest);
+      }
+    }
+
+    const targetUuid = xcodeProject.getFirstTarget().uuid;
     const buildPhase = xcodeProject.pbxSourcesBuildPhaseObj(targetUuid);
     const alreadyAdded = Object.values(buildPhase.files || {}).some(f =>
       typeof f === 'object' && f.comment && f.comment.includes('VisionOCR')
@@ -18,12 +32,10 @@ module.exports = function withVisionOCR(config) {
     ];
 
     for (const { name, type } of files) {
-      // Generate deterministic UUIDs
       const seed = name.split('').reduce((a, c) => a + c.charCodeAt(0), 0);
       const fileRefUuid = `AA${seed.toString(16).padStart(6, '0').toUpperCase()}00000000000000000001`;
       const buildFileUuid = `AA${seed.toString(16).padStart(6, '0').toUpperCase()}00000000000000000002`;
 
-      // Add PBXFileReference
       xcodeProject.pbxFileReferenceSection()[fileRefUuid] = {
         isa: 'PBXFileReference',
         lastKnownFileType: type,
@@ -33,7 +45,6 @@ module.exports = function withVisionOCR(config) {
       };
       xcodeProject.pbxFileReferenceSection()[`${fileRefUuid}_comment`] = name;
 
-      // Add PBXBuildFile
       xcodeProject.pbxBuildFileSection()[buildFileUuid] = {
         isa: 'PBXBuildFile',
         fileRef: fileRefUuid,
@@ -41,11 +52,9 @@ module.exports = function withVisionOCR(config) {
       };
       xcodeProject.pbxBuildFileSection()[`${buildFileUuid}_comment`] = `${name} in Sources`;
 
-      // Add to Sources build phase
       buildPhase.files.push({ value: buildFileUuid, comment: `${name} in Sources` });
 
-      // Add to main group
-      const mainGroup = xcodeProject.pbxGroupByName('TCGCodeScanner');
+      const mainGroup = xcodeProject.pbxGroupByName('CodesTCG');
       if (mainGroup) {
         mainGroup.children.push({ value: fileRefUuid, comment: name });
       }
